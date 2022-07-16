e8c8921cc9869ba58f508f9da376dc9a328e1d88
syadav <syadav@gozego.com>
2022-07-16 21:06:49 +0530

    Initial project setup

---
+++ src/IndexScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const IndexScreen = () =>{
+    return (
+        <View>
+            <Text>My index page</Text>
+        </View>
+
+    );    
+};
+
+
+const styles = StyleSheet.create({
+
+});
+
+
+
+export default IndexScreen;
