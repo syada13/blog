3dff5f8106c9fbfd78a64d2dd0693bd2fefeb308
syadav <syadav@gozego.com>
2022-07-21 18:13:08 +0530

    Add trash icon against each blog and apply styles

--- src/IndexScreen.js
+++ src/IndexScreen.js
@@ -1,21 +1,23 @@
 import React,{ useContext } from 'react';
 import {View,Text,StyleSheet,FlatList,Button} from 'react-native';
 import { Context } from './context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
     const {state,addBlogPost} = useContext(Context);
     return (
         <View>
-            <Text>My index page</Text>
             <Button title="Add Post" onPress={() => addBlogPost()}
             />
             <FlatList
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({item}) => {
-                    return <Text>{item.title}</Text>
-                 }
-                }
+                    return <View style={styles.row}>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Feather style={styles.icon} name="trash"/>
+                    </View>
+                }}
             />
         </View>
     );    
@@ -23,6 +25,27 @@ const IndexScreen = () => {
 
 
 const styles = StyleSheet.create({
+    row:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        borderTopWidth:1,
+        borderColor: 'gray',
+        paddingHorizontal: 10,
+        backgroundColor:'green'
+
+    },
+    title: {
+        fontSize: 12,
+        color:'white'
+
+
+    },
+    icon:{
+      fontSize: 24,
+      color:'black'
+
+    }
 });
 
 
