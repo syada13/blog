a9c2afcee881339f6e891fca099f246180029d02
syadav <syadav@gozego.com>
2022-07-24 16:15:25 +0530

    Implementing logic to delete a blogPost

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,9 +3,11 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state,action) =>{
     switch (action.type) {
+        case 'delete_blogpost':           
+         return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'add_blogpost':
-        return [...state,{title: `Blog Posts#${state.length +1}`}];
-        default:
+         return [...state,{id: Math.floor(Math.random() * 99999), title: `Blog Posts#${state.length +1}`}];
+         default:
             return state;
     }
     
@@ -18,9 +20,19 @@ const addBlogPost = dispatch => {
     };  
 };
 
+
+const deleteBlogPost = dispatch => {
+
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id});
+
+    };
+
+};
+
 export const {Context, Provider} = createDataContext(
     blogReducer,
-    { addBlogPost },
+    { addBlogPost,deleteBlogPost},
     []
     );
 
